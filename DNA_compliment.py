2f1de93eff30707693e6aef5404a7c15ae515b8c
Aarish Sajid <161928829+AarishSajid@users.noreply.github.com>
2024-07-17 02:22:37 +0500

    Update DNA_compliment.py

--- DNA_compliment.py
+++ DNA_compliment.py
@@ -1,7 +1,10 @@
 dna = open('rosalind/data.txt').read()
+#opening dataset given by rosalind
 compliment = ''
+#empty string to append changed nucleotides onto
 
 for nucleotide in dna[::-1]:
+#for loop running in reverse 
     if nucleotide == 'A':
         nucleotide = 'T'
     elif nucleotide == 'T':
@@ -10,7 +13,10 @@ for nucleotide in dna[::-1]:
         nucleotide = 'G'
     elif nucleotide == 'G':
         nucleotide = 'C'
+#changes to nucleotides 
     compliment += nucleotide
+#apppend to string 
 
 print(compliment)
+#and ur done
     
